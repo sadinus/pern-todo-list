b66d119fb033a57f64560f2f991d51aa8636fa97
Paweł Pindelski <pindelski.pawel@gmail.com>
2023-11-30 15:00:01 +0100

    DB setup + simple CRUD operations

---
+++ server/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE perntodolist;
+
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
+});
+
+module.exports = pool;
