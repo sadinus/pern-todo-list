0a2cf5168e4c508b96a08a01f378775126366803
Paweł Pindelski <pindelski.pawel@gmail.com>
2023-12-05 14:28:18 +0100

    Finished UI

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import InputTodo from "../components/InputTodo";
+import ListTodos from "../components/ListTodos";
+
+function App() {
+  return (
+    <div className="container">
+      <InputTodo />
+      <ListTodos />
+    </div>
+  );
+}
+
+export default App;
